dcb88493b470706e38bb179cce93af1333972d84
Matheus Moraes <matheusmoraesdj2025@gmail.com>
2026-08-13 17:42:06 -0300

    fix: add visual viewport hook for tablet sidebar

---
+++ src/hooks/use-visual-viewport.ts
@@ -0,0 +1,30 @@
+import * as React from "react"
+
+export function useVisualViewport() {
+  const [height, setHeight] = React.useState<number | null>(null)
+
+  React.useEffect(() => {
+    const visualViewport = window.visualViewport
+
+    if (!visualViewport) {
+      return
+    }
+
+    const updateHeight = () => {
+      setHeight(Math.round(visualViewport.height))
+    }
+
+    updateHeight()
+    visualViewport.addEventListener("resize", updateHeight)
+    visualViewport.addEventListener("scroll", updateHeight)
+    window.addEventListener("resize", updateHeight)
+
+    return () => {
+      visualViewport.removeEventListener("resize", updateHeight)
+      visualViewport.removeEventListener("scroll", updateHeight)
+      window.removeEventListener("resize", updateHeight)
+    }
+  }, [])
+
+  return height
+}
