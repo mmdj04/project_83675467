12d60c7f0c4e1c20d989f9c254b81de2a73ca0c4
rj-vulgo22 <matheusmoraesdj2025@gmail.com>
2026-08-14 20:38:51 +0000

    refactor: simplify landing page with centered hero and dashboard iframe

--- src/app/(external)/page.tsx
+++ src/app/(external)/page.tsx
@@ -1,25 +1,11 @@
-import { AccessibilityTroubleshooting } from "./_components/accessibility-troubleshooting";
-import { Features } from "./_components/features";
-import { Footer } from "./_components/footer";
 import { Hero } from "./_components/hero";
-import { Installation } from "./_components/installation";
-import { Nav } from "./_components/nav";
-import { Stats } from "./_components/stats";
-import { TechStack } from "./_components/tech-stack";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col">
-      <Nav />
       <main className="flex-1">
         <Hero />
-        <Stats />
-        <Features />
-        <Installation />
-        <TechStack />
-        <AccessibilityTroubleshooting />
       </main>
-      <Footer />
     </div>
   );
 }
